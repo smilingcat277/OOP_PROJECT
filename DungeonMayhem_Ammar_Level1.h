0c524000c830a86b81e5958e39922c4fd66e8acf
Smiling cat <ammarsyedali7@gmail.com>
2025-05-11 23:38:30 +0500

    Update DungeonMayhem_Ammar_Level.h

--- DungeonMayhem_Ammar_Level1.h
+++ DungeonMayhem_Ammar_Level1.h
@@ -2,4 +2,6 @@
 #include <SFML/Graphics.hpp>
 
 void setupLevelText(sf::Text& text, sf::Font& font);
-void drawMap1(sf::RenderWindow& window, const int map1[10][16], float TILE_SIZE);
+void drawMap1(sf::RenderWindow& window, const int map1[10][16], float TILE_SIZE, sf::Texture &brickTexture, sf::Texture &textureGround);
+void drawMap2(sf::RenderWindow& window, const int map2[20][9], float TILE_SIZE, sf::Texture &brickTexture ,sf::Texture &GroundTexture, sf::Vector2f offset);
+void drawMap3(sf::RenderWindow& window, const int map3[12][17], float TILE_SIZE, sf::Texture &brickTexture ,sf::Texture &GroundTexture, sf::Vector2f offset);
