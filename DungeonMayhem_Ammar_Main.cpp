4383e616e86137f3257f9cd7f8aa3ecfcd810ae0
Smiling cat <ammarsyedali7@gmail.com>
2025-05-11 16:10:06 +0500

    Update DungeonMayhem_Ammar_Main.cpp

--- DungeonMayhem_Ammar_Main.cpp
+++ DungeonMayhem_Ammar_Main.cpp
@@ -4,9 +4,17 @@
 #include "player.h"
 #include "level.h"
 
-int main()
-{   
-    sf::RenderWindow window(sf::VideoMode(1500, 1000), "SFML works!");
+int main(){
+
+    bool level1Complete = false;
+    bool level2Complete = false;
+    bool transitioningCenter = false;
+    sf::Vector2f targetCenter;
+    bool transitioningView = false;
+    sf::Vector2f targetViewSize;
+    float viewTransitionSpeed = 0.5f;
+
+    sf::RenderWindow window(sf::VideoMode(1600, 1000), "SFML works!");
     const float TILE_SIZE = 100.f;
 
     sf::Font font;
@@ -17,11 +25,17 @@ int main()
     player.setPosition(sf::Vector2f(0.f, 0.f));
     player.setFillColor(sf::Color::White);
 
-    sf::RectangleShape rectangle2(sf::Vector2f(1600.f, 1000.f));
-    rectangle2.setPosition(sf::Vector2f(1600.f, 0.f));
-    rectangle2.setFillColor(sf::Color(100, 100, 100));
+    sf::RectangleShape door(sf::Vector2f(100.f, 100.f));
+    door.setPosition(sf::Vector2f(1900.f, -1050.f));
+    door.setFillColor(sf::Color::Red);
 
-    bool levelComplete = false;
+    sf::RectangleShape map2(sf::Vector2f(700.f, 2000.f));
+    map2.setPosition(sf::Vector2f(1600.f, -1000.f));
+    map2.setFillColor(sf::Color(0xADD8E6));
+
+    sf::RectangleShape map3(sf::Vector2f(1600.f, 1000.f));
+    map3.setPosition(sf::Vector2f(1150.f, -2000.f));
+    map3.setFillColor(sf::Color(255, 192, 203));
 
     sf::View camera;
     camera.setSize(sf::Vector2f(1000.f, 900.f));
@@ -35,20 +49,60 @@ int main()
                 window.close();
         }
 
-        camera.setCenter(player.getPosition());
+        if (!transitioningView && !level2Complete){
+            camera.setCenter(player.getPosition());
+        }
+
         window.setView(camera);
 
+        if (level2Complete && !transitioningView){
+            transitioningView = true;
+            targetViewSize = sf::Vector2f(1650.f, 1050.f);
+        }
+
+        if (transitioningView){
+
+            sf::Vector2f currentSize = camera.getSize();
+
+            currentSize += (targetViewSize - currentSize) * 0.05f * viewTransitionSpeed;
+
+            camera.setSize(currentSize);
+
+            if (std::abs(currentSize.x - targetViewSize.x) < 1.f && std::abs(currentSize.y - targetViewSize.y) < 1.f){
+                camera.setSize(targetViewSize);
+                transitioningView = false;
+
+                transitioningCenter = true;
+                targetCenter = sf::Vector2f(1950.f, -1475.f); // center of map3
+            }
+        }
+
+        if (transitioningCenter) {
+            sf::Vector2f currentCenter = camera.getCenter();
+            currentCenter += (targetCenter - currentCenter) * 0.05f * viewTransitionSpeed;
+            camera.setCenter(currentCenter);
+
+            if (std::abs(currentCenter.x - targetCenter.x) < 1.f && std::abs(currentCenter.y - targetCenter.y) < 1.f){
+                camera.setCenter(targetCenter);
+                transitioningCenter = false;
+            }
+        }
+
         handlePlayerMovement(player);
-        handlePlayerCollision(player, levelComplete);
+
+        handlePlayerCollision(player, level1Complete, level2Complete);
 
         window.clear();
 
         drawMap1(window, map1, TILE_SIZE);
 
-        window.draw(rectangle2);
+        window.draw(map2);
+        window.draw(map3);
+        window.draw(door);
         window.draw(text1);
         window.draw(player);
         window.display();
     }
+
     return 0;
 }
